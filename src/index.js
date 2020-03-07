12d358bfad9c0808e3b490182b06be534bab0ed3
Graham <graham.butler@ecorventures.com>
2020-03-06 20:29:07 -0800

    Refactor
    Removed some unnecessary lines of code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import Command from './command.js'
 import Shell from './shell.js'
-
-const all = {
-  Command,
-  Shell
-}
-
-export default Object.assign({}, { Command, Shell })
-export { Command, Shell }
+export { Command, Shell, { Command, Shell } as default }
