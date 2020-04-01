20583b45c1730394f4bc3445eb358cf4051730b8
Corey Butler <corey@coreybutler.com>
2020-03-31 23:14:40 -0500

    Updated npm module to support sourcemap files.

--- build/lib/browser-package.js
+++ build/lib/browser-package.js
@@ -16,7 +16,7 @@ build.supportedBrowsers().forEach(edition => {
 
   const prefix = `${config.npmOrganization}${config.npmOrganization.trim().length > 0 ? '/' : ''}`
   outdir = `${config.browserOutput}/${edition}`
-  const srcfile = `${config.browserOutput}/browser-${build.name}/${build.name}-${build.version}${edition.replace('browser-'+build.name, '')}.min.js`
+  const srcfile = `${config.browserOutput}/browser-${build.name}/${build.name}-${build.version}${edition.replace('browser-' + build.name, '')}.min.js`
 
   console.log(`Creating production npm package for ${edition}`)
   const pkg = Object.assign({}, mainpkg)
@@ -24,8 +24,9 @@ build.supportedBrowsers().forEach(edition => {
   pkg.description = `${mainpkg.description} (Production code for ${originalEditionName} browsers)`
   pkg.devDependencies = {}
   pkg.devDependencies[`${prefix}${edition}-debug`] = mainpkg.version
-  pkg.module = `./${build.name}-${build.version}${edition.replace('browser-'+build.name, '')}.min.js`
-  pkg.main = `./${build.name}-${build.version}${edition.replace('browser-'+build.name, '')}.min.js`
+  pkg.files.indexOf('*.js.map') < 0 && pkg.files.push('*.js.map')
+  pkg.module = `./${build.name}-${build.version}${edition.replace('browser-' + build.name, '')}.min.js`
+  pkg.main = `./${build.name}-${build.version}${edition.replace('browser-' + build.name, '')}.min.js`
 
   if (!fs.existsSync(outdir)) {
     fs.mkdirSync(outdir)
@@ -43,10 +44,10 @@ build.supportedBrowsers().forEach(edition => {
       .toString()
       .replace(/\/+#\s+?sourceMappingURL=/, `//# sourceMappingURL=../${edition}-debug/`)
     fs.writeFileSync(globalFile, content)
-    fs.renameSync(srcfile.replace('.min.js', '-global.min.js'), `${outdir}/${build.name}-${build.version}${edition.replace('browser-'+build.name, '')}-global.min.js`)
+    fs.renameSync(srcfile.replace('.min.js', '-global.min.js'), `${outdir}/${build.name}-${build.version}${edition.replace('browser-' + build.name, '')}-global.min.js`)
   }
 
-  fs.renameSync(srcfile, `${outdir}/${build.name}-${build.version}${edition.replace('browser-'+build.name, '')}.min.js`)
+  fs.renameSync(srcfile, `${outdir}/${build.name}-${build.version}${edition.replace('browser-' + build.name, '')}.min.js`)
   fs.writeFileSync(`${outdir}/package.json`, JSON.stringify(pkg, null, 2))
 
   // Add license
@@ -57,7 +58,7 @@ build.supportedBrowsers().forEach(edition => {
 
   console.log(`Creating debug npm package for ${edition}.`)
   fs.mkdirSync(`${outdir}-debug`)
-  fs.renameSync(srcfile.replace('.min.js', '.min.js.map'), `${outdir}-debug/${build.name}-${build.version}${edition.replace('browser-'+build.name, '')}.min.js.map`)
+  fs.renameSync(srcfile.replace('.min.js', '.min.js.map'), `${outdir}-debug/${build.name}-${build.version}${edition.replace('browser-' + build.name, '')}.min.js.map`)
   if (originalEditionName === 'current') {
     fs.renameSync(srcfile.replace('.min.js', '-global.min.js.map'), `${outdir}-debug/${build.name}-${build.version}-global.min.js.map`)
   }
@@ -66,8 +67,9 @@ build.supportedBrowsers().forEach(edition => {
   delete pkg.devDependencies
   pkg.name = `${pkg.name}-debug`
   pkg.description = `${mainpkg.description} (Sourcemaps for ${originalEditionName} browsers)`
-  pkg.module = `./${build.name}-${build.version}${edition.replace('browser-'+build.name, '')}.min.js.map`
-  pkg.main = `./${build.name}-${build.version}${edition.replace('browser-'+build.name, '')}.min.js.map`
+  pkg.module = `./${build.name}-${build.version}${edition.replace('browser-' + build.name, '')}.min.js.map`
+  pkg.main = `./${build.name}-${build.version}${edition.replace('browser-' + build.name, '')}.min.js.map`
+  pkg.files.indexOf('*.js.map') < 0 && pkg.files.push('*.js.map')
   pkg.dependencies = {}
   pkg.dependencies['source-map-support'] = mainpkg['source-map-support']
   fs.writeFileSync(`${outdir}-debug/package.json`, JSON.stringify(pkg, null, 2))
