7de541fe83483c33956136174c3db1d43aef2a24
Corey Butler <corey@coreybutler.com>
2020-05-04 15:35:50 -0500

    Updated reset process to not remove the .github folder

--- build/lib/reset.js
+++ build/lib/reset.js
@@ -5,7 +5,7 @@ fs
   .readdirSync(path.resolve('./'))
   .forEach(filepath => {
     const fullpath = path.resolve(filepath)
-    if (filepath.startsWith('.') && filepath !== '.git' && fs.statSync(fullpath).isDirectory()) {
+    if (filepath.startsWith('.') && filepath !== '.github' && filepath !== '.git' && fs.statSync(fullpath).isDirectory()) {
       fs.rmdirSync(filepath, { recursive: true })
       console.log(`Removed ${fullpath}`)
     }
